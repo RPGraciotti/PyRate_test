15b684d7afcb72ed928aa4bba62b1724581db416
Hugo Pereira <hugodavid@usp.br>
2020-03-29 20:31:43 -0300

    adicionando variáveis

--- loop_reach.sh
+++ loop_reach.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+n1 = 30000000
+n2 = 10000
 for i in {1..5}
 do
-python PyRateContinuous.py -d Canidae_all_$i\_Grj_se_est.txt -m 0 -c reach$i.txt -n 30000000 -s 10000 -stimes 30 21 -equal_G 0 -equal_R 0 > error_rb$i.txt
+python PyRateContinuous.py -d Canidae_all_$i\_Grj_se_est.txt -m 0 -c reach$i.txt -n $n1 -s $n2 -stimes 30 21 -equal_G 0 -equal_R 0 > error_rb$i.txt
 done
