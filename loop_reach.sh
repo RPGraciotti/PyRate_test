93d088ba487efd7709f8a8e3d070e54f39558768
Hugo Pereira <hugodavid@usp.br>
2020-04-03 10:29:25 -0300

    RESOLVIDO PORRA

--- loop_reach.sh
+++ loop_reach.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-n1 = 30000000
-n2 = 10000
-for i in {1..5}
-do
-python PyRateContinuous.py -d Canidae_all_$i\_Grj_se_est.txt -m 0 -c reach$i.txt -n $n1 -s $n2 -stimes 30 21 -equal_G 0 -equal_R 0 > error_rb$i.txt
-done
+seq 10 | xargs -I{} -n 1 -P 2 python PyRateContinuous.py -d Canidae_all_{}_Grj_se_est.txt -m 0 -c reach{}.txt -n 30000 -s 1000 -stimes 30 21 -equal_G 0 -equal_R 0 > error_rb{}.txt
+
