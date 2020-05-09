9c03e7421184eaeddd6f02c2e1531e56f75d29f7
Rodolfo Pereira Graciotti <rpgraciotti@labmeme1.local>
2020-05-09 20:38:13 -0300

    testar o {}

---
+++ MPD/reach_mpd.sh
@@ -0,0 +1,20 @@
+python PyRateContinuous.py -d Canidae_all_1_Grj_se_est.txt -m 0 -c reach_mpd_both1.txt -n 30000000 -s 10000 -stimes 34 26 17 6 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_b1.txt &
+python PyRateContinuous.py -d Canidae_all_2_Grj_se_est.txt -m 0 -c reach_mpd_both2.txt -n 30000000 -s 10000 -stimes 34 26 17 6 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_b2.txt &
+python PyRateContinuous.py -d Canidae_all_3_Grj_se_est.txt -m 0 -c reach_mpd_both3.txt -n 30000000 -s 10000 -stimes 34 26 17 6 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_b3.txt &
+python PyRateContinuous.py -d Canidae_all_4_Grj_se_est.txt -m 0 -c reach_mpd_both4.txt -n 30000000 -s 10000 -stimes 34 26 17 6 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_b4.txt &
+python PyRateContinuous.py -d Canidae_all_5_Grj_se_est.txt -m 0 -c reach_mpd_both5.txt -n 30000000 -s 10000 -stimes 34 26 17 6 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_b5.txt &
+python PyRateContinuous.py -d Canidae_all_6_Grj_se_est.txt -m 0 -c reach_mpd_both6.txt -n 30000000 -s 10000 -stimes 34 26 17 6 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_b6.txt &
+python PyRateContinuous.py -d Canidae_all_7_Grj_se_est.txt -m 0 -c reach_mpd_both7.txt -n 30000000 -s 10000 -stimes 34 26 17 6 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_b7.txt &
+python PyRateContinuous.py -d Canidae_all_8_Grj_se_est.txt -m 0 -c reach_mpd_both8.txt -n 30000000 -s 10000 -stimes 34 26 17 6 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_b8.txt &
+python PyRateContinuous.py -d Canidae_all_9_Grj_se_est.txt -m 0 -c reach_mpd_both9.txt -n 30000000 -s 10000 -stimes 34 26 17 6 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_b9.txt &
+python PyRateContinuous.py -d Canidae_all_10_Grj_se_est.txt -m 0 -c reach_mpd_both10.txt -n 30000000 -s 10000 -stimes 34 26 17 6 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_b10.txt &
+python PyRateContinuous.py -d Canidae_all_1_Grj_se_est.txt -m 0 -c reach_mpd_both1.txt -n 30000000 -s 10000 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_nb1.txt &
+python PyRateContinuous.py -d Canidae_all_2_Grj_se_est.txt -m 0 -c reach_mpd_both2.txt -n 30000000 -s 10000 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_nb2.txt &
+python PyRateContinuous.py -d Canidae_all_3_Grj_se_est.txt -m 0 -c reach_mpd_both3.txt -n 30000000 -s 10000 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_nb3.txt &
+python PyRateContinuous.py -d Canidae_all_4_Grj_se_est.txt -m 0 -c reach_mpd_both4.txt -n 30000000 -s 10000 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_nb4.txt &
+python PyRateContinuous.py -d Canidae_all_5_Grj_se_est.txt -m 0 -c reach_mpd_both5.txt -n 30000000 -s 10000 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_nb5.txt &
+python PyRateContinuous.py -d Canidae_all_6_Grj_se_est.txt -m 0 -c reach_mpd_both6.txt -n 30000000 -s 10000 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_nb6.txt &
+python PyRateContinuous.py -d Canidae_all_7_Grj_se_est.txt -m 0 -c reach_mpd_both7.txt -n 30000000 -s 10000 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_nb7.txt &
+python PyRateContinuous.py -d Canidae_all_8_Grj_se_est.txt -m 0 -c reach_mpd_both8.txt -n 30000000 -s 10000 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_nb8.txt &
+python PyRateContinuous.py -d Canidae_all_9_Grj_se_est.txt -m 0 -c reach_mpd_both9.txt -n 30000000 -s 10000 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_nb9.txt &
+python PyRateContinuous.py -d Canidae_all_10_Grj_se_est.txt -m 0 -c reach_mpd_both10.txt -n 30000000 -s 10000 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_nb10.txt &
