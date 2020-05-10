09a491e7d5e932f36112df83f2409121b4e98cb8
Rodolfo Pereira Graciotti <rpgraciotti@labmeme1.local>
2020-05-09 21:10:45 -0300

    removi parametro -A 1 que mata com a ram

--- MPD/loop_reach_MPD.sh
+++ MPD/loop_reach_MPD.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-seq 10 | xargs -I{} -n 1 -P 10 python PyRateContinuous.py -d Canidae_all_{}_Grj_se_est.txt -m 0 -c reach_mpd_both{}.txt -n 30000 -s 1000 -stimes 34 26 17 6 -equal_G 0 -equal_R 0 -A 1 > error_reach_mpd_b{}.txt
+seq 10 | xargs -I{} -n 1 -P 10 python PyRateContinuous.py -d Canidae_all_{}_Grj_se_est.txt -m 0 -c reach_mpd_both{}.txt -n 30000 -s 1000 -stimes 34 26 17 6 -equal_G 0 -equal_R 0 > error_reach_mpd_b{}.txt
 
